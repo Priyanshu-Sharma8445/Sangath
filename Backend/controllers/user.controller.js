60ee881ff44f937c230c57815d0eb35afb2bed8f
Priyanshu-Sharma8445 <priyanshu.sharma.mzn@gmail.com>
2024-12-23 12:20:30 +0530

    editProfile logic adsed and cloudinary is seted up

--- Backend/controllers/user.controller.js
+++ Backend/controllers/user.controller.js
@@ -1,6 +1,8 @@
 import { User } from "../models/user.model.js";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken.js"
+import getDataUri from "../config/datauri.js";
+import cloudinary from "../config/cloudinary.js";
 
 export const register = async (req, res) => {
     try {
@@ -124,3 +126,43 @@ export const getProfile = async (req,res)=>{
         console.log(error)
     }
 }
+
+export const editProfile = async (req,res)=>{
+    try {
+        const userId = req.id;
+        const{bio,gender} = req.body;
+        const profilePicture = req.file;
+
+        let cloudResponse;
+
+        if(profilePicture){
+            const fileUri = getDataUri(profilePicture);
+            cloudResponse = await cloudinary.uploader.upload(fileUri);
+        }
+
+        const user = await User.findById(userId);;
+        if(!user) 
+        {
+            return res.status(404).json({
+                message : 'User not found',
+                success : false
+            })
+        }
+
+        if(bio) user.bio = bio;
+        if(gender) user.gender = gender;
+        if(profilePicture) user.profilePicture = cloudResponse.secure_url;
+
+        await user.save();
+
+        return res.status(200).json({
+            message : 'Profile updated',
+            success : true,
+            user
+        })
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+
