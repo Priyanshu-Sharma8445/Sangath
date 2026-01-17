d30c2207cdf936e617cd9e0ce51bc1669e62112e
Priyanshu-Sharma8445 <priyanshu.sharma.mzn@gmail.com>
2026-01-17 19:43:03 +0530

    final conversion of rendering or serving through backend for deployment

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectdb = async ()=>{
     try{        
-        await mongoose.connect(process.env.Mongo_URI)
+        await mongoose.connect(process.env.Mongo_URI);
         console.log("databse connected");
     }catch(error){
         console.log(error)
